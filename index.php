20782ae90b1a4106d58f535e99a942d8df663916
王杰 <915120623@qq.com>
2019-11-18 14:09:32 +0800

    注入代码

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
+checkSignature();
+// 检查签名
+function checkSignature()
+{
+    $signature = $_GET["signature"];
+    $timestamp = $_GET["timestamp"];
+    $nonce = $_GET["nonce"];
+
+    $token = '123456';
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr);
+    $tmpStr = implode( $tmpArr );
+    $tmpStr = sha1( $tmpStr );
+
+    if( $tmpStr == $signature ){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
 $postArr =  file_get_contents("php://input");
 print_r($postArr);
 
