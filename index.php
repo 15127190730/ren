704b95dc21668cee184631706f6f4e56febc8d41
王杰 <915120623@qq.com>
2019-11-18 15:15:14 +0800

    注入代码

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/WXBizMsgCrypt.php';
+require_once 'wxBizMsgCrypt.php';
 
 define("AppID", "wxb9b907dd8e4bf31f");
 define("AppSecret", "2b1dd565cfad6a3b6cc069d5dcb4c369");
@@ -28,12 +28,12 @@ if ($tmpStr == $signature) {
 $msg_signature = $_GET['msg_signature'];
 $encrypt_type  = (isset($_GET['encrypt_type']) && ($_GET['encrypt_type'] == 'aes')) ? "aes" : "raw";
 $postArr = file_get_contents("php://input");
-//if ($encrypt_type == 'aes') {
-//    $pc         = new WXBizMsgCrypt(TOKEN, EncodingAESKey, AppID);
-//    $decryptMsg = "";  //解密后的明文
-//    $errCode    = $pc->DecryptMsg($msg_signature, $timestamp, $nonce, $postArr, $decryptMsg);
-//    $postArr    = $decryptMsg;
-//}
+if ($encrypt_type == 'aes') {
+    $pc         = new WXBizMsgCrypt(TOKEN, EncodingAESKey, AppID);
+    $decryptMsg = "";  //解密后的明文
+    $errCode    = $pc->DecryptMsg($msg_signature, $timestamp, $nonce, $postArr, $decryptMsg);
+    $postArr    = $decryptMsg;
+}
 
 file_put_contents('aaa.txt', $postArr, FILE_APPEND);
 
