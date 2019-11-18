7ec32d7610fdb485e9c5064451de4772e0edd041
王杰 <915120623@qq.com>
2019-11-18 17:44:42 +0800

    注入代码

--- index.php
+++ index.php
@@ -1,64 +1,66 @@
 <?php
 include_once "wxBizMsgCrypt.php";
 define("AppSecret", "2b1dd565cfad6a3b6cc069d5dcb4c369");
+responseMsg();
+function responseMsg() {
+    $appId          = 'wxb9b907dd8e4bf31f';
+    $encodingAesKey = '39tWlDKmDQVxH8nPEd49TzGGAS1pKhmAj0sEbnIYWz0';
+    $token          = "laopifu";
 
-$appId          = 'wxb9b907dd8e4bf31f';
-$encodingAesKey = '39tWlDKmDQVxH8nPEd49TzGGAS1pKhmAj0sEbnIYWz0';
-$token          = "laopifu";
+    $signature = $_GET["signature"];
+    $timestamp = $_GET["timestamp"];
+    $nonce     = $_GET["nonce"];
 
-$signature = $_GET["signature"];
-$timestamp = $_GET["timestamp"];
-$nonce     = $_GET["nonce"];
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr);
+    $tmpStr = implode($tmpArr);
+    $tmpStr = sha1($tmpStr);
 
-$tmpArr = array($token, $timestamp, $nonce);
-sort($tmpArr);
-$tmpStr = implode($tmpArr);
-$tmpStr = sha1($tmpStr);
-
-if ($tmpStr == $signature) {
-    $echostr = $_GET["echostr"];
-    echo $echostr;
-} else {
-    return false;
-}
-
-
-$msg_signature = $_GET['msg_signature'];
-$encrypt_type  = (isset($_GET['encrypt_type']) && ($_GET['encrypt_type'] == 'aes')) ? "aes" : "raw";
-$postArr       = file_get_contents("php://input");
-if (!empty($postArr)) {
-    if ($encrypt_type == 'aes') {
-        $pc         = new WXBizMsgCrypt($token, $encodingAesKey, $appId);
-        $decryptMsg = "";  //解密后的明文
-        $errCode    = $pc->decryptMsg($msg_signature, $timestamp, $nonce, $postArr, $decryptMsg);
-        if ($errCode == 0) {
-            $postArr = $decryptMsg;
-        }
-
+    if ($tmpStr == $signature) {
+        $echostr = $_GET["echostr"];
+        echo $echostr;
+    } else {
+        return false;
     }
-}
-$postObj = simplexml_load_string($postArr); //将xml数据转换为对象
-if(strtolower($postObj->MsgType) == 'text'){
-    $keyword = trim($postObj->Content);
 
-    if(!empty($keyword)) {
-        if($keyword=='123'){
-            $content='老匹夫';
+
+    $msg_signature = $_GET['msg_signature'];
+    $encrypt_type  = (isset($_GET['encrypt_type']) && ($_GET['encrypt_type'] == 'aes')) ? "aes" : "raw";
+    $postArr       = file_get_contents("php://input");
+    if (!empty($postArr)) {
+        if ($encrypt_type == 'aes') {
+            $pc         = new WXBizMsgCrypt($token, $encodingAesKey, $appId);
+            $decryptMsg = "";  //解密后的明文
+            $errCode    = $pc->decryptMsg($msg_signature, $timestamp, $nonce, $postArr, $decryptMsg);
+            if ($errCode == 0) {
+                $postArr = $decryptMsg;
+            }
+
         }
     }
+    $postObj = simplexml_load_string($postArr); //将xml数据转换为对象
+    if (strtolower($postObj->MsgType) == 'text') {
+        $keyword = trim($postObj->Content);
+
+        if (!empty($keyword)) {
+            if ($keyword == '123') {
+                $content = '老匹夫';
+            }
+        }
+    }
+
+    if (isset($content) && $content) {
+        $info = responseText($postObj, $content);
+        //加密
+        if ($encrypt_type == 'aes') {
+            $encryptMsg = ''; //加密后的密文
+            $errCode    = $pc->encryptMsg($info, $timestamp, $nonce, $encryptMsg);
+            $info       = $encryptMsg;
+        }
+        echo $info;
+    }
 }
 
-if(isset($content) && $content) {
-    $info = responseText($postObj, $content);
-    //加密
-    if ($encrypt_type == 'aes') {
-        $encryptMsg = ''; //加密后的密文
-        $errCode = $pc->encryptMsg($info, $timestamp, $nonce, $encryptMsg);
-        $info = $encryptMsg;
-    }
-    echo $info;
-}
-file_put_contents('aaa.txt', $info, FILE_APPEND);
 //回复文字消息
 function responseText($postObj, $content) {
     $toUser   = $postObj->FromUserName;
