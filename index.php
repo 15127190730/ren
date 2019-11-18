16859fe87471ee54ef4e3ef05fde4020eb1f449c
王杰 <915120623@qq.com>
2019-11-18 16:10:08 +0800

    注入代码

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 include_once "wxBizMsgCrypt.php";
 define("AppSecret", "2b1dd565cfad6a3b6cc069d5dcb4c369");
 
-$appId='wxb9b907dd8e4bf31f';
-$encodingAesKey='39tWlDKmDQVxH8nPEd49TzGGAS1pKhmAj0sEbnIYWz0';
-$token="laopifu";
+$appId          = 'wxb9b907dd8e4bf31f';
+$encodingAesKey = '39tWlDKmDQVxH8nPEd49TzGGAS1pKhmAj0sEbnIYWz0';
+$token          = "laopifu";
 
 $signature = $_GET["signature"];
 $timestamp = $_GET["timestamp"];
@@ -31,8 +31,8 @@ if (!empty($postArr)) {
         $pc         = new WXBizMsgCrypt($token, $encodingAesKey, $appId);
         $decryptMsg = "";  //解密后的明文
         $errCode    = $pc->decryptMsg($msg_signature, $timestamp, $nonce, $postArr, $decryptMsg);
-        if($errCode==0){
-            $postArr    = $decryptMsg;
+        if ($errCode == 0) {
+            $postArr = $decryptMsg;
         }
 
     }
@@ -40,11 +40,10 @@ if (!empty($postArr)) {
 $postObj = simplexml_load_string($postArr); //将xml数据转换为对象
 file_put_contents('aaa.txt', $postArr, FILE_APPEND);
 //回复文字消息
-function responseText($postObj, $content)
-{
-    $toUser = $postObj->FromUserName;
+function responseText($postObj, $content) {
+    $toUser   = $postObj->FromUserName;
     $fromUser = $postObj->ToUserName;
-    $time = time();
+    $time     = time();
     //返回文字消息的模板
     $textTpl = "<xml>
                     <ToUserName><![CDATA[%s]]></ToUserName>
@@ -53,7 +52,8 @@ function responseText($postObj, $content)
                     <MsgType><![CDATA[text]]></MsgType>
                     <Content><![CDATA[%s]]></Content>
                     </xml>";
-    $info = sprintf($textTpl, $toUser, $fromUser, $time, $content);
+    $info    = sprintf($textTpl, $toUser, $fromUser, $time, $content);
     return $info;
 }
+
 ?>
