8d4b8a45dfa4f6e60a08ca7d0c98c04bd32473ed
王杰 <915120623@qq.com>
2019-11-18 16:41:11 +0800

    注入代码

--- pkcs7Encoder.php
+++ pkcs7Encoder.php
@@ -71,18 +71,12 @@ class Prpcrypt {
             //获得16位随机字符串，填充到明文之前
             $random = $this->getRandomStr();
             $text   = $random . pack("N", strlen($text)) . $text . $appid;
-            // 网络字节序
-            $size   = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
             $iv     = substr($this->key, 0, 16);
             //使用自定义的填充方式对明文进行补位填充
             $pkc_encoder = new PKCS7Encoder;
             $text        = $pkc_encoder->encode($text);
-            mcrypt_generic_init($module, $this->key, $iv);
-            //加密
-            $encrypted = mcrypt_generic($module, $text);
-            mcrypt_generic_deinit($module);
-            mcrypt_module_close($module);
+            $encrypted = openssl_encrypt($text, 'AES-256-CBC', $this->key, OPENSSL_RAW_DATA, $this->hexToStr($iv));
+
 
             //print(base64_encode($encrypted));
             //使用BASE64对加密后的字符串进行编码
