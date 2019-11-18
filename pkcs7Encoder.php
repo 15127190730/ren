43d1f628fca34cceffac9cab4da20418425ebf52
王杰 <915120623@qq.com>
2019-11-18 16:13:21 +0800

    注入代码

--- pkcs7Encoder.php
+++ pkcs7Encoder.php
@@ -99,7 +99,6 @@ class Prpcrypt {
      * @return string 解密得到的明文
      */
     public function decrypt($encrypted, $appid) {
-        file_put_contents('aaa.txt', '王杰王杰王杰王杰王杰王杰王杰王杰', FILE_APPEND);
         try {
             //使用BASE64对需要解密的字符串进行解码
             $ciphertext_dec = base64_decode($encrypted);
