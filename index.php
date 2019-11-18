09d816596a33c9767a1b0fcc6e8915e25b050941
王杰 <915120623@qq.com>
2019-11-18 18:27:25 +0800

    注入代码

--- index.php
+++ index.php
@@ -4,12 +4,16 @@ define("AppSecret", "2b1dd565cfad6a3b6cc069d5dcb4c369");
 define("AppID", "wxb9b907dd8e4bf31f");
 define("TOKEN", "laopifu");
 define("EncodingAESKey", "39tWlDKmDQVxH8nPEd49TzGGAS1pKhmAj0sEbnIYWz0");
-checkSignature();
+//checkSignature();
 responseMsg();
 
 //$appId          = 'wxb9b907dd8e4bf31f';
 //$encodingAesKey = '39tWlDKmDQVxH8nPEd49TzGGAS1pKhmAj0sEbnIYWz0';
 //$token          = "laopifu";
+/**
+ * @return bool
+ * 验签
+ */
 function checkSignature() {
     $signature = $_GET["signature"];
     $timestamp = $_GET["timestamp"];
@@ -36,15 +40,15 @@ function responseMsg() {
     $encrypt_type  = (isset($_GET['encrypt_type']) && ($_GET['encrypt_type'] == 'aes')) ? "aes" : "raw";
     $postArr       = file_get_contents("php://input");
     if (!empty($postArr)) {
-        if ($encrypt_type == 'aes') {
-            $pc         = new WXBizMsgCrypt(TOKEN, EncodingAESKey, AppID);
-            $decryptMsg = "";  //解密后的明文
-            $errCode    = $pc->decryptMsg($msg_signature, $timestamp, $nonce, $postArr, $decryptMsg);
-            if ($errCode == 0) {
-                $postArr = $decryptMsg;
-            }
-
-        }
+//        if ($encrypt_type == 'aes') {
+//            $pc         = new WXBizMsgCrypt(TOKEN, EncodingAESKey, AppID);
+//            $decryptMsg = "";  //解密后的明文
+//            $errCode    = $pc->decryptMsg($msg_signature, $timestamp, $nonce, $postArr, $decryptMsg);
+//            if ($errCode == 0) {
+//                $postArr = $decryptMsg;
+//            }
+//
+//        }
         $postObj = simplexml_load_string($postArr); //将xml数据转换为对象
         /*判断消息类型并选择回复信息*/
         if (strtolower($postObj->MsgType) == 'text') {
@@ -59,17 +63,18 @@ function responseMsg() {
 
         if (isset($content) && $content) {
             $info = responseText($postObj, $content);
+            echo $info;
             //加密
-            if ($encrypt_type == 'aes') {
-                $encryptMsg = ''; //加密后的密文
-                $pc         = new WXBizMsgCrypt(TOKEN, EncodingAESKey, AppID);
-                $errCode    = $pc->encryptMsg($info, $timestamp, $nonce, $encryptMsg);
-                if ($errCode == 0) {
-                    echo $encryptMsg;
-//                    file_put_contents('aaa.txt', $encryptMsg, FILE_APPEND);
-                }
-
-            }
+//            if ($encrypt_type == 'aes') {
+//                $encryptMsg = ''; //加密后的密文
+//                $pc         = new WXBizMsgCrypt(TOKEN, EncodingAESKey, AppID);
+//                $errCode    = $pc->encryptMsg($info, $timestamp, $nonce, $encryptMsg);
+//                if ($errCode == 0) {
+//                    echo $encryptMsg;
+////                    file_put_contents('aaa.txt', $encryptMsg, FILE_APPEND);
+//                }
+//
+//            }
         }
     }
 
